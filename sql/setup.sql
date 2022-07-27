a58ee7fae0ec6626c02ef41bd40a0930709a8269
sirdan <stanford303@gmail.com>
2022-07-27 14:03:40 -0700

    made changes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,24 @@
+DROP table if exists cats;
+
+CREATE table cats (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  type VARCHAR NOT NULL,
+  year INT,
+  lives INT,
+  isSidekick VARCHAR NOT NULL
+);
+
+INSERT INTO cats (name, type, year, lives, isSidekick) VALUES
+('felix', 'tuxedo', 1892, 3, 'false'),
+('Garfield', 'Orange Tabby', 1978, 7, 'false'),
+('Duchess', 'Angora', 1970, 9, 'false'),
+('stimpy', 'Manx', 1990, 1, 'true');
+
+
+
+
+
 -- Use this file to define your SQL tables
+
 -- The SQL in this file will be executed when you run `npm run setup-db`
